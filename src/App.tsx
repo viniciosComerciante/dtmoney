54a553c1b8f3da48cbbb6805e8f898e6908a183d
viniciosComerciante <vinicompif@gmail.com>
2022-02-05 18:42:45 -0300

    adicionados estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./assets/styles/globals";
+
 export function App() {
   return (
+    
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello world</h1>
+      <GlobalStyle/>
     </div>
   );
 }
