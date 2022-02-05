769901bafca1a4f99e381c0bfab4dc719de1b06d
viniciosComerciante <vinicompif@gmail.com>
2022-02-05 18:47:17 -0300

    adicionado variaveis de cores globais

--- src/assets/styles/globals.ts
+++ src/assets/styles/globals.ts
@@ -4,6 +4,15 @@ export const GlobalStyle = createGlobalStyle`
 
     :root{
         --background: #f0f2f5;
+        --red: #e52e4d;
+        --blue: #5429cc;
+
+        --blue-light: #6933ff;
+        
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+
+        --shape: #ffffff;
     }
 
     *{
@@ -21,10 +30,19 @@ export const GlobalStyle = createGlobalStyle`
         }
     }
     
-    
+
 
     body{
         background: var(----background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    [disabled]{
+        opacity: 0.6;
+        cursor: not-allowed;
     }
 `
