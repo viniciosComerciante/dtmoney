870b7c2d24aabc8d07583cd8013a9827e208b3d9
viniciosComerciante <vinicompif@gmail.com>
2022-02-15 00:53:25 -0300

    formataçao de valores concluida

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -38,9 +38,18 @@ export const TransactionsTable = () => {
               return (
                 <tr key={transaction.id}>
                   <td>{transaction.title}</td>
-                  <td className={transaction.type}>R$12.000</td>
-                  <td>Desenvolvimento</td>
-                  <td>20/02/2021</td>
+                  <td className={transaction.type}>
+                    {new Intl.NumberFormat("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    }).format(transaction.amount)}
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {new Intl.DateTimeFormat("pt-BR").format(
+                      new Date(transaction.createdAt)
+                    )}
+                  </td>
                 </tr>
               );
             })}
